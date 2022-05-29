16f2369413a729442f80168952da1f6a815ea685
tobyw121 <tobiasw763@gmail.com>
2022-05-29 15:55:31 +0200

    Add files via upload

---
+++ step2.sh
@@ -0,0 +1,9 @@
+#!/bin/ash
+
+apk add lightdm lightdm-gtk-greeter dbus dbus-x11  xfce4 xfce4-terminal lightdm lightdm-gtk-greeter
+apk add mesa-gl glib feh firefox-esr accountsservice openvpn
+
+
+setup-xorg-base
+
+
---
+++ step3.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+
+rc-service lightdm start
+rc-update add lightdm
+
+reboot
+
+
+
+
